55a24f634cc5515df24db163b3a450815bf421e5
陈永超 <chen_yongchao@126.com>
2024-10-24 17:58:51 +0800

    fix: 调整线上nest地址

--- src/api/pageSchema.ts
+++ src/api/pageSchema.ts
@@ -1,10 +1,15 @@
+import { env, host } from '@/components/use-schema-render/config';
+
 /**
  * 查找页面schema
  */
 export async function findPageSchemaByNodeId(params: { nodeId: number }) {
-  const response = await fetch(`/api/page-schema/${params.nodeId}`, {
-    method: 'get',
-  });
+  const response = await fetch(
+    `${env === 'localhost' ? '/api' : host}/page-schema/${params.nodeId}`,
+    {
+      method: 'get',
+    }
+  );
   const res = await response.json();
   if (res.code == 1) {
     res.data.package = JSON.parse(res.data.package || '[]');
